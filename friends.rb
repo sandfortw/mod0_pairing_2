d769ad9ee54bcf08a4a1576d0eb7ce47874ff680
Weston Sandfort <sandfortw@gmail.com>
2022-11-02 15:37:40 -0600

    Add an elsif

--- friends.rb
+++ friends.rb
@@ -1,7 +1,11 @@
-friend = "Bobby"
+friend = "Drew"
+friend2 = "Drew"
 if
     friend == "Jack"
     puts "Sup buddy?"
+elsif
+    friend == friend2
+    puts "Suuuuup buddddddy???"
 else
     puts "Hey other friend..."
 end
